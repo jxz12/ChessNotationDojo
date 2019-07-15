f9635bb66e1354afb95082f01ec9609187f92177
Jonathan Zheng <jz@jonatharbookpro.lan>
2019-07-15 01:27:34 +0100

    yayyy

---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+////////////////////////////////////////////////////////
+// a class to save all the information of a chess game
+// TODO: can also evaluate moves and play with other engines
+
+public class Engine
+{
+    public Engine(int ranks=8, int files=8)
+    {
+        nRanks = ranks;
+        nFiles = files;
+    }
+
+    public static int nRanks = 8;
+    public static int nFiles = 8;
+    private Move previous { get; set; } = null;
+
+    public HashSet<int> WhitePawns   { get; private set; } = new HashSet<int> { 8,9,10,11,12,13,14,15 };
+    public HashSet<int> WhiteRooks   { get; private set; } = new HashSet<int> { 0,7 };
+    public HashSet<int> WhiteKnights { get; private set; } = new HashSet<int> { 1,6 };
+    public HashSet<int> WhiteBishops { get; private set; } = new HashSet<int> { 2,5 };
+    public HashSet<int> WhiteQueens  { get; private set; } = new HashSet<int> { 3 };
+    public HashSet<int> WhiteKings   { get; private set; } = new HashSet<int> { 4 };
+
+    public HashSet<int> BlackPawns   { get; private set; } = new HashSet<int> { 48,49,50,51,52,53,54,55 };
+    public HashSet<int> BlackRooks   { get; private set; } = new HashSet<int> { 56,63 };
+    public HashSet<int> BlackKnights { get; private set; } = new HashSet<int> { 57,62 };
+    public HashSet<int> BlackBishops { get; private set; } = new HashSet<int> { 58,61 };
+    public HashSet<int> BlackQueens  { get; private set; } = new HashSet<int> { 59 };
+    public HashSet<int> BlackKings   { get; private set; } = new HashSet<int> { 60 };
+
+    // a class to store all the information needed for a move
+    // a Move plus the board state is all the info needed for move generation
+    private class Move
+    {
+        public bool whiteMove;
+
+        public enum PieceType : byte { Pawn, Rook, Knight, Bishop, Queen, King };
+        public PieceType Piece { get; set; }
+        public int Source { get; set; }
+        public int Target { get; set; }
+
+        public enum MoveType : byte { Quiet, Capture, Check, Castle, EnPassant };
+        public MoveType Type { get; set; }
+        public PieceType Promotion { get; set; }
+
+        public string Algebraic {
+            get
+            {
+                // TODO: other piece types and stuff
+                return (char)('a'+(Source%nFiles)) + "" + ((Target/nFiles)+1);
+            }
+        }
+    }
+
+    // Generate moves given the current board state and previous move
+    private List<Move> GenerateMoves()
+    {
+        var moves = new List<Move>();
+        if (previous == null || !previous.whiteMove)
+        {
+            foreach (int pawn in WhitePawns)
+            {
+                if (pawn / nFiles < nRanks-1)
+                {
+                    var newMove = new Move();
+                    newMove.whiteMove = true;
+                    newMove.Piece = Move.PieceType.Pawn;
+                    newMove.Source = pawn;
+                    newMove.Target = pawn + nFiles;
+                    newMove.Type = Move.MoveType.Quiet;
+                    // newMove.promotion = Move.PieceType.Pawn;
+                    moves.Add(newMove);
+                }
+            }
+        }
+        else
+        {
+            foreach (int pawn in BlackPawns)
+            {
+                if (pawn / nFiles > 0)
+                {
+                    var newMove = new Move();
+                    newMove.whiteMove = false;
+                    newMove.Piece = Move.PieceType.Pawn;
+                    newMove.Source = pawn;
+                    newMove.Target = pawn - nFiles;
+                    newMove.Type = Move.MoveType.Quiet;
+                    // newMove.promotion = Move.PieceType.Pawn;
+                    moves.Add(newMove);
+                }
+            }
+        }
+        return moves;
+    }
+    private void PerformMove(Move move)
+    {
+        if (move.Piece == Move.PieceType.Pawn)
+        {
+            if (move.whiteMove)
+            {
+                WhitePawns.Remove(move.Source);
+                WhitePawns.Add(move.Target);
+            }
+            else
+            {
+                BlackPawns.Remove(move.Source);
+                BlackPawns.Add(move.Target);
+            }
+            previous = move;
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    ///////////////////////////////////
+    // for interface from the outside
+
+    public IEnumerable<string> GetMovesAlgebraic()
+    {
+        var moves = GenerateMoves();
+        // UnityEngine.Debug.Log(moves.Count);
+        foreach (Move move in moves)
+        {
+            yield return move.Algebraic;
+        }
+    }
+
+    public void PerformMoveAlgebraic(string todo)
+    {
+        var moves = GenerateMoves();
+        foreach (Move move in moves)
+        {
+            if (move.Algebraic == todo)
+            {
+                PerformMove(move);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	// TODO: options to make invisible/pulse
+	//       flip board
+	//       parse input
+	//       record games to text
+	//       color schemes
+
+	[SerializeField] List<Text> squares; // TODO: change to a class Square and make it any size
+	[SerializeField] Button N, B, R, Q, K, eq;
+	[SerializeField] List<Button> files;
+	[SerializeField] List<Button> ranks;
+	private Engine thomas;
+	void Awake()
+	{
+		thomas = new Engine(ranks.Count, files.Count);
+	}
+
+	private HashSet<string> allCandidates;
+	private string candidate;
+	void Start()
+	{
+		N.onClick.AddListener(()=> InputChar('N'));
+		B.onClick.AddListener(()=> InputChar('B'));
+		R.onClick.AddListener(()=> InputChar('R'));
+		Q.onClick.AddListener(()=> InputChar('Q'));
+		K.onClick.AddListener(()=> InputChar('K'));
+		eq.onClick.AddListener(()=> InputChar('='));
+		for (int i=0; i<files.Count; i++)
+		{
+			char input = (char)('a'+i);
+			files[i].onClick.AddListener(()=> InputChar(input)); // lol
+		}
+		for (int i=0; i<ranks.Count; i++)
+		{
+			char input = (char)('1'+i);
+			ranks[i].onClick.AddListener(()=> InputChar(input)); // lol
+		}
+
+		candidate = "";
+		allCandidates = new HashSet<string>(thomas.GetMovesAlgebraic());
+		ShowPossibleChars();
+		Display();
+	}
+	void ShowPossibleChars()
+	{
+		N.interactable = false;
+		B.interactable = false;
+		R.interactable = false;
+		Q.interactable = false;
+		K.interactable = false;
+		eq.interactable = false;
+		foreach (Button b in files)
+		{
+			b.interactable = false;
+		}
+		foreach (Button b in ranks)
+		{
+			b.interactable = false;
+		}
+
+		int idx = candidate.Length;
+		foreach (string move in allCandidates)
+		{
+			if (move.Length <= idx || move.Substring(0,idx) != candidate)
+			{
+				continue;
+			}
+			// print(move + " " + c);
+			char c = move[idx];
+
+			if (c >= 'a' && c <= 'h')
+			{
+				files[c - 'a'].interactable = true;
+			}
+			else if (c >= '1' && c <= '8')
+			{
+				ranks[c - '1'].interactable = true;
+			}
+			else if (c == 'N')
+			{
+				N.interactable = true;
+			}
+			else if (c == 'B')
+			{
+				B.interactable = true;
+			}
+			else if (c == 'R')
+			{
+				R.interactable = true;
+			}
+			else if (c == 'Q')
+			{
+				Q.interactable = true;
+			}
+			else if (c == 'K')
+			{
+				K.interactable = true;
+			}
+			else if (c == '=')
+			{
+				eq.interactable = true;
+			}
+		}
+	}
+	public void InputChar(char input)
+	{
+		candidate += input;
+		if (allCandidates.Contains(candidate))
+		{
+			// print(candidate);
+			thomas.PerformMoveAlgebraic(candidate);
+			candidate = "";
+			allCandidates = new HashSet<string>(thomas.GetMovesAlgebraic());
+			Display();
+		}
+		ShowPossibleChars();
+	}
+
+	void ClearBoard()
+	{
+		foreach (Text square in squares)
+		{
+			square.text = "";
+		}
+	}
+	void DisplayPiece(IEnumerable<int> positions, string letter, Color colour)
+	{
+		foreach (int i in positions)
+		{
+			squares[i].color = colour;
+			squares[i].text = letter;
+		}
+	}
+	void Display()
+	{
+		ClearBoard();
+		DisplayPiece(thomas.WhitePawns,   "p", Color.white);
+		DisplayPiece(thomas.WhiteRooks,   "R", Color.white);
+		DisplayPiece(thomas.WhiteKnights, "N", Color.white);
+		DisplayPiece(thomas.WhiteBishops, "B", Color.white);
+		DisplayPiece(thomas.WhiteQueens,  "Q", Color.white);
+		DisplayPiece(thomas.WhiteKings,   "K", Color.white);
+		DisplayPiece(thomas.BlackPawns,   "p", Color.black);
+		DisplayPiece(thomas.BlackRooks,   "R", Color.black);
+		DisplayPiece(thomas.BlackKnights, "N", Color.black);
+		DisplayPiece(thomas.BlackBishops, "B", Color.black);
+		DisplayPiece(thomas.BlackQueens,  "Q", Color.black);
+		DisplayPiece(thomas.BlackKings,   "K", Color.black);
+	}
+}
